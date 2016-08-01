dda15b6a7da1a5efa16f37ae11d4a066ac9027e9
Kyle McWeeney <kyle.mcweeney22@gmail.com>
2016-08-01 01:50:59 -0400

    updated config.php file

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
-$API_KEY = 'Put your API key here'; // This will be your Flickr-issued API Key.
-$PAGE_TITLE = 'Kittens and Rainbows'; // This should be a user-friendly page title.
-$SEARCH_TAGS = 'kitten,rainbow'; // This must be a comma-separated list
+$API_KEY = '6766378d78a45ffe57958f08681ab5ba'; // This will be your Flickr-issued API Key.
+$PAGE_TITLE = 'The Beauty in Nature'; // This should be a user-friendly page title.
+$SEARCH_TAGS = 'nature,outdoors,wonderland,paradise,beautiful'; // This must be a comma-separated list
 
 ?>
